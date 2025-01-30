a567bad75cc93d89eeebbb689e12175afb0ca988
Eliana Ḿendez <eliamargi@gmail.com>
2025-01-30 17:34:19 -0500

    Agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+
+    if(amigos !== ""){
+        //obtengo el dato ingresado en el input
+        let amigo = document.getElementById('amigo').value;
+    
+        //mostrar lista de amigos
+        document.getElementById('listaAmigos').textContent = amigo; 
+        limpiarCaja();
+    } else {
+        alert('Por favor inserte un nombre');
+    }
+   
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+ 
+
